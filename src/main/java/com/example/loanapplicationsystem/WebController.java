4d84e00d63c3694ad5e98f0411493e34f21b637e
Alfie <alfredokang@gmail.com>
2020-02-07 11:18:10 -0500

    Update WebController.java

--- src/main/java/com/example/loanapplicationsystem/WebController.java
+++ src/main/java/com/example/loanapplicationsystem/WebController.java
@@ -1,22 +1,5 @@
 package com.example.loanapplicationsystem;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-//Validation imports
 import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -27,12 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
 public class WebController implements WebMvcConfigurer {
-	
-	  @Value("${spring.datasource.url}")
-	  private String dbUrl;
-
-	  @Autowired
-	  private DataSource dataSource;
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
@@ -53,36 +30,4 @@ public class WebController implements WebMvcConfigurer {
 
 		return "redirect:/results";
 	}
-	
-	@RequestMapping("/db")
-	String db(Map<String, Object> model) {
-		try (Connection connection = dataSource.getConnection()) {
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-			ArrayList<String> output = new ArrayList<String>();
-			while (rs.next()) {
-			output.add("Read from DB: " + rs.getTimestamp("tick"));
-			}
-
-			model.put("records", output);
-			return "db";
-		} catch (Exception e) {
-			model.put("message", e.getMessage());
-			return "error";
-		}
-	}
-	
-	  @Bean
-	  public DataSource dataSource() throws SQLException {
-	  	if (dbUrl == null || dbUrl.isEmpty()) {
-	  		return new HikariDataSource();
-	  } else {
-			HikariConfig config = new HikariConfig();
-			config.setJdbcUrl(dbUrl);
-			return new HikariDataSource(config);
-	 	}
-	 }
 }
