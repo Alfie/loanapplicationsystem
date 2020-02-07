e1b211575e637894d4fe3b311111d62ddef84b7f
Alfie <alfredokang@gmail.com>
2020-02-07 11:24:23 -0500

    Update DatabaseController.java

--- src/main/java/com/example/loanapplicationsystem/DatabaseController.java
+++ src/main/java/com/example/loanapplicationsystem/DatabaseController.java
@@ -11,6 +11,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
