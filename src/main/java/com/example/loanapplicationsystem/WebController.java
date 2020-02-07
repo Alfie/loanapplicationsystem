e253f37e7dd33374b29a1701b24391e6d4f016c0
Alfie <alfredokang@gmail.com>
2020-02-07 10:46:42 -0500

    Update WebController.java

--- src/main/java/com/example/loanapplicationsystem/WebController.java
+++ src/main/java/com/example/loanapplicationsystem/WebController.java
@@ -1,5 +1,8 @@
 package com.example.loanapplicationsystem;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -44,21 +47,32 @@ public class WebController implements WebMvcConfigurer {
 	@RequestMapping("/db")
 	String db(Map<String, Object> model) {
 		try (Connection connection = dataSource.getConnection()) {
-		Statement stmt = connection.createStatement();
-		stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-		stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-		ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+			Statement stmt = connection.createStatement();
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+			stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+			ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-		ArrayList<String> output = new ArrayList<String>();
-		while (rs.next()) {
-		output.add("Read from DB: " + rs.getTimestamp("tick"));
-		}
+			ArrayList<String> output = new ArrayList<String>();
+			while (rs.next()) {
+			output.add("Read from DB: " + rs.getTimestamp("tick"));
+			}
 
-		model.put("records", output);
-		return "db";
+			model.put("records", output);
+			return "db";
 		} catch (Exception e) {
-		model.put("message", e.getMessage());
-		return "error";
+			model.put("message", e.getMessage());
+			return "error";
 		}
 	}
+	
+	  @Bean
+	  public DataSource dataSource() throws SQLException {
+	  	if (dbUrl == null || dbUrl.isEmpty()) {
+	  		return new HikariDataSource();
+	  } else {
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(dbUrl);
+			return new HikariDataSource(config);
+	 	}
+	 }
 }
