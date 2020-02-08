66c821dab1a36b0f2afe2f074e90f1f25703e271
Alfie <alfredokang@gmail.com>
2020-02-08 16:05:23 -0500

    Update and rename PersonForm.java to Loan.java

--- src/main/java/com/example/loanapplicationsystem/Loan.java
+++ src/main/java/com/example/loanapplicationsystem/Loan.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-public class PersonForm {
+public class Loan {
 
 	@NotNull
 	@Size(min=2, max=30)
