d18f6665edb0a8c3cc8376165b5cbf21d79e6015
Alfie <alfredokang@gmail.com>
2020-02-09 04:21:38 -0500

    Update Loan.java

--- src/main/java/com/example/loanapplicationsystem/Loan.java
+++ src/main/java/com/example/loanapplicationsystem/Loan.java
@@ -4,6 +4,12 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Loan {
 
 	@NotNull
@@ -13,6 +19,15 @@ public class Loan {
 	@NotNull
 	@Min(18)
 	private Integer age;
+	
+	@Id
+    	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    	public Long getId() {
+        	return id;
+    	}
+	
+	protected Loan() {
+    	}
 
 	public String getName() {
 		return this.name;
