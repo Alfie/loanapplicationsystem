a21f72290da2c15e1c72eab82d59fcad52d66fa6
Alfie <alfredokang@gmail.com>
2020-02-09 02:39:59 -0500

    Create LoanService.java

---
+++ src/main/java/com/example/loanapplicationsystem/LoanService.java
@@ -0,0 +1,29 @@
+import java.util.List;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+ 
+@Service
+@Transactional
+public class LoanService {
+ 
+    @Autowired
+    private LoanRepository repo;
+     
+    public List<Loan> listAll() {
+        return repo.findAll();
+    }
+     
+    public void save(Loan loan) {
+        repo.save(loan);
+    }
+     
+    public Loan get(long id) {
+        return repo.findById(id).get();
+    }
+     
+    public void delete(long id) {
+        repo.deleteById(id);
+    }
+}
